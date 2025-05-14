bb7b3c7b0bded2e8224d853ed09d274232d32137
Shehryyy <143545797+Shehryyy@users.noreply.github.com>
2025-05-14 18:26:35 -0400

    Update items2.dart

--- appdevproject/lib/JsonModels/items2.dart
+++ appdevproject/lib/JsonModels/items2.dart
@@ -5,6 +5,7 @@ class Items2 {
   final String type;
   final String neededBy;
   final int userId;
+  final int isActive;
 
   Items2({
     this.itemId,
@@ -13,28 +14,30 @@ class Items2 {
     required this.type,
     required this.neededBy,
     required this.userId,
+    this.isActive = 1,
   });
 
-  Map<String,dynamic> toMap() {
+  Map<String, dynamic> toMap() {
     return {
-      'itemId' : itemId,
-      'itemName'  : itemName,
-      'quantity' : quantity,
-      'type' : type,
-      'neededBy' : neededBy,
-      "userId" : userId
+      'itemId': itemId,
+      'itemName': itemName,
+      'quantity': quantity,
+      'type': type,
+      'neededBy': neededBy,
+      'userId': userId,
+      'isActive': isActive,
     };
   }
 
   factory Items2.fromMap(Map<String, dynamic> map) {
     return Items2(
-        itemId: map['itemId'],
-        itemName: map['itemName'],
-        quantity: map['quantity'],
-        type: map['type'],
-        neededBy: map['neededBy'],
-        userId: map['userId']
+      itemId: map['itemId'],
+      itemName: map['itemName'],
+      quantity: map['quantity'],
+      type: map['type'],
+      neededBy: map['neededBy'],
+      userId: map['userId'],
+      isActive: map['isActive'] ?? 1,
     );
   }
-
 }
